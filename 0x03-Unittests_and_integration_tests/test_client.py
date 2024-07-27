135d3869c3b03245839f105c39cfa070a624c9b4
FeirieO <feirie001@gmail.com>
2024-07-27 19:09:30 +0100

    updated test_public class

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -34,16 +34,16 @@ class TestGithubOrgClient(unittest.TestCase):
             self.assertEqual(test_return, mock_get.return_value.get("repos_url"))
             mock_get.assert_called_once()
 
-    @patch("client.get_json", return_value=[{"name": "holberton"}])
+    @patch("client.get_json", return_value=[{"name": "repo1"}, {"name": "repo2"}])
     def test_public_repos(self, mock_get):
         """ to unit-test GithubOrgClient.public_repos """
         with patch.object(GithubOrgClient,
                           "_public_repos_url",
                           new_callable=PropertyMock,
-                          return_value="https://api.github.com/") as mock_pub:
-            test_client = GithubOrgClient("holberton")
+                          return_value="https://api.github.com/orgs/google/repos") as mock_pub:
+            test_client = GithubOrgClient("google")
             test_return = test_client.public_repos()
-            self.assertEqual(test_return, ["holberton"])
+            self.assertEqual(test_return, ["repo1", "repo2"])
             mock_get.assert_called_once()
             mock_pub.assert_called_once()
 
